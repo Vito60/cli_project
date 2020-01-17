c4001bbb69e51a3afcc7fcf6011ec1ce55adee38
Randall Williams <randallw1996@gmail.com>
2020-01-17 12:31:47 -0500

    update cli

---
+++ lib/Pokemon_cli_app.rb
@@ -0,0 +1,6 @@
+require "Pokemon_cli_app/version"
+
+module PokemonCliApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Pokemon_cli_app/version.rb
@@ -0,0 +1,3 @@
+module PokemonCliApp
+  VERSION = "0.1.0"
+end
